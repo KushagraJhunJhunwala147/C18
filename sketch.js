e19d62cc6ea90be60068c377ee137eaf897e6313
KushagraJhunJhunwala147 <88532339+KushagraJhunJhunwala147@users.noreply.github.com>
2021-10-30 14:05:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(400,400);
+  Student1=new Student ("Kushagra",13,7)
+}
+
+function draw() 
+{
+  background(30);
+ Student1.display ()
+ 
+ 
+}
---
+++ student.js
@@ -0,0 +1,27 @@
+
+class Student
+{
+ constructor (name,age,gread) {
+   this.name=name
+   this.age=age
+   this.gread=gread
+ }  
+  display(){
+    console.log (this.name)
+    console.log (this.age)
+    console.log (this.gread)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
